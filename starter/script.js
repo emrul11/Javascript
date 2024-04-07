0b4c7c6f137a0899b5e6c241ec7e4d474d9f5dc2
emrul11 <emrulkabir111@gmail.com>
2024-04-07 16:00:51 +0600

    update

--- starter/script.js
+++ starter/script.js
@@ -15,8 +15,8 @@ console.log(document.querySelector('.guess').value);
 */
 
 let secreatNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -31,6 +31,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secreatNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
